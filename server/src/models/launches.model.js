609b331c93113815598a65c359cef982959ed5ee
Yash Gupta <yash8740gupta@gmail.com>
2025-05-27 16:06:14 +0530

    Adding mongo models for both planets and launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+//const launches = require('./launches.mongo');
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
@@ -8,7 +10,7 @@ const launch = {
     rocket: 'Explorer IS1',
     launchDate: new Date('December 28, 2030'),
     target: 'Keplar-442 b',
-    customer: ['Yash', 'NASA'],
+    customers: ['Yash', 'NASA'],
     upcoming: true,
     success: true,
 };
