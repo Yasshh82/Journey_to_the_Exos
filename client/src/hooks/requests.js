eaccdf8b8c76daaaab76a1eac1073cc6a499810c
Yash Gupta <yash8740gupta@gmail.com>
2025-05-30 01:02:18 +0530

    Adding api router

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,20 +1,21 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000/v1';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
-  // Load planets and return as JSON.
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber;
   });
-  // Load launches, sort by flight number, and return as JSON.
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
@@ -30,7 +31,6 @@ async function httpSubmitLaunch(launch) {
       ok: false,
     };
   }
-  // Submit given launch data to launch system.
 }
 
 async function httpAbortLaunch(id) {
